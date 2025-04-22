410b3c41bfadf43dd9c934c6e83c871e05164dd9
ehghks021203 <ehghks021203@gmail.com>
2025-04-22 19:17:00 +0900

    feat: uv 환경으로 마이그레이션

---
+++ app/db/base.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+# 앞으로 모델 추가되면 여기서 import
+
+from sqlalchemy.orm import declarative_base
+Base = declarative_base()
---
+++ app/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config.database import USER_NAME, USER_PW, DATABASE_NAME
+
+SQLALCHEMY_DATABASE_URL = f"mysql://{USER_NAME}:{USER_PW}@localhost:3306/{DATABASE_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
